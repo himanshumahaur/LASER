5d5f1706a3e26026fef956e8c78b827c8b678ed6
himanshumahaur <himanshumahaur@outlook.com>
2026-01-23 14:35:32 +0000

    refactor

--- src/main.py
+++ src/main.py
@@ -1,28 +1,17 @@
 class Node:
-    def __init__(self, id, battery = 100):
+    def __init__(self, id, x, y, energy):
         self.id = id
-        self.battery = battery
+        self.x = x
+        self.y = y
+        self.energy = energy
 
-    def tick(self):
-        print('tick')
+        self.isCH = False
+        self.cluster = None
 
 class Network:
     def __init__(self, nodes):
         self.nodes = nodes
 
-    def tick(self):
-        for node in self.nodes:
-            node.tick()
-
-class NetworkUtils:
-    def createNetwork(self, nodes):
-        network = Network(nodes)
-        return network
 
 if __name__ == '__main__':
-    n1 = Node(1)
-    n2 = Node(2)
-    n3 = Node(3)
-
-    net1 = Network([n1, n2, n3])
-    net1.tick()
+    print('LASER')
